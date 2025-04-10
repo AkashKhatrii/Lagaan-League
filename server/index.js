86f65e10b06979808c3e0ee24fe364c49380af89
AkashKhatrii <akashkhatri1001@gmail.com>
2025-04-10 03:16:16 -0600

    import path module

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const Game = require("./models/Game");
 const Player = require("./models/Player");
 const bcrypt = require("bcryptjs");
-
+const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
